54032729334f9ab4922c86b7ed77e93b899db5ec
Ruslan Babin <vasus714@yandex.ru>
2023-09-04 11:10:46 +0300

    [test] map2ex

---
+++ Test.Tests/map2ex.fs
@@ -0,0 +1,56 @@
+module Test.Tests
+
+open System
+open NUnit.Framework
+open FsUnit
+
+open Test.Map2ex
+
+let infSeqOfX x =
+    seq {
+        while true do
+            yield x
+    }
+
+[<Test>]
+let ``test infinite seqs`` () =
+    let seqOfOnes = infSeqOfX 1
+    let seqOfTwos = infSeqOfX 2
+    let seqOfThree = infSeqOfX 3
+
+    let mapped = map2ex seqOfOnes seqOfTwos (+)
+
+    should equal <|| (Seq.take 10 seqOfThree, Seq.take 10 mapped)
+
+[<Test>]
+let ``test left seq len is less`` () =
+    let left = [ 1; 2 ] |> Seq.ofList
+    let right = [ 4; 5; 6 ] |> Seq.ofList
+
+    let result = map2ex left right (*)
+
+    (fun () -> ignore <| List.ofSeq result) |> should throw typeof<Exception>
+
+[<Test>]
+let ``test right seq len is less`` () =
+    let left = [ 4; 5; 6 ] |> Seq.ofList
+    let right = [ 1; 2 ] |> Seq.ofList
+
+    let result = map2ex left right (*)
+
+    (fun () -> ignore <| List.ofSeq result) |> should throw typeof<Exception>
+
+[<Test>]
+let ``test seqs lengths are equals`` () =
+    let left = [ 4; 5; 6 ] |> Seq.ofList
+    let right = [ 1; 2; 3 ] |> Seq.ofList
+
+    let result = List.ofSeq <| map2ex left right (*)
+
+    should equal [ 4; 10; 18 ] result
+
+[<Test>]
+let ``test empty seqs`` () =
+    let result = List.ofSeq <| map2ex [] [] (*)
+
+    should equal Seq.empty result
---
+++ Test/map2ex.fs
@@ -0,0 +1,36 @@
+﻿module Test.Map2ex
+
+let inline xor a b = (a || b) && not (a && b)
+
+
+let rec map2ex (fst: 'a seq) (snd: 'b seq) (fn: 'a -> 'b -> 'c) =
+    seq {
+        if not ((Seq.isEmpty fst) && (Seq.isEmpty snd)) then
+            if Seq.isEmpty fst || Seq.isEmpty snd then
+                failwith "lengths of sequences must be equal"
+
+            yield fn (Seq.head fst) (Seq.head snd)
+
+            yield! map2ex (Seq.skip 1 fst) (Seq.skip 1 snd) fn
+    }
+
+let seqOfOnes =
+    seq {
+        while true do
+            yield 1
+    }
+
+let seqOfTwos =
+    seq {
+        while true do
+            yield 2
+    }
+
+// ТЕБЕ НЕ ПОКАЖУ!!!! ТЫ ПИСЬКА !!!! КУДА СМОТРИШЬ!!!
+
+let s1 = [ 1; 2; 3 ] |> Seq.ofList
+let s2 = [ 4; 5; 6; 7 ] |> Seq.ofList
+// let kek = map2ex s1 s2 <| (*)
+let kek = map2ex seqOfOnes seqOfTwos <| (+)
+
+printf "%A" <| Seq.take 3 kek
